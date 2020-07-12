e34334ed6d6df0cbfe9666ea37e94d1c014868be
Ritu Mundhe <ritumunday@gmail.com>
2020-07-12 12:58:31 -0500

    include bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 function App() {
   return (
